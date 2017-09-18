8f476a6131808704440c16de8587c242381a00d2
Andy Diamondstein <adiamondstein@google.com>
2017-09-18 09:18:49 -0400

    Add file that will hold helper functions

---
+++ helper.js
@@ -0,0 +1,5 @@
+    sprites = new Image();
+    sprites.src = 'https://raw.githubusercontent.com/natetarrh/frogger/master/assets/frogger_sprites.png';
+    sprites.onload = function() {
+      alert('sprites loaded');
+    };
