97b550f54f5f4f94f4a14ea0e8803a62e16549a7
Andy Diamondstein <adiamondstein@google.com>
2017-09-18 14:35:25 -0400

    Update test1.js

--- test1.js
+++ test1.js
@@ -1,4 +1,12 @@
-  function test_one() {
-    alert('trying test one again');
-  }
-  test_one();
+function test_one() {
+  alert('trying test one again');
+}
+
+helpSprites = new Image();
+helpSprites.src = 'https://raw.githubusercontent.com/natetarrh/frogger/master/assets/frogger_sprites.png';
+helpSprites.onload = function() {
+  alert('sprites loaded');
+  $('#testp').html('why');
+};
+
+test_one();
