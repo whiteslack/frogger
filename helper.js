89fe2ea6eec0f4883ae4152e80a6a5f9b624f637
Andy Diamondstein <adiamondstein@google.com>
2017-09-18 09:24:00 -0400

    Update helper.js

--- helper.js
+++ helper.js
@@ -1,5 +1,6 @@
-    sprites = new Image();
-    sprites.src = 'https://raw.githubusercontent.com/natetarrh/frogger/master/assets/frogger_sprites.png';
-    sprites.onload = function() {
-      alert('sprites loaded');
-    };
+alert('try loading sprites');
+sprites = new Image();
+sprites.src = 'https://raw.githubusercontent.com/natetarrh/frogger/master/assets/frogger_sprites.png';
+sprites.onload = function() {
+  alert('sprites loaded');
+};
