6237e536cf72410d1744642e395b54bbd6a8d3b1
Andy Diamondstein <adiamondstein@google.com>
2017-09-18 09:45:33 -0400

    Try to make function callable

--- helper.js
+++ helper.js
@@ -1,14 +1,20 @@
-alert('try loading sprites');
-helpSprites = new Image();
-helpSprites.src = 'https://raw.githubusercontent.com/natetarrh/frogger/master/assets/frogger_sprites.png';
-helpSprites.onload = function() {
-  alert('sprites loaded');
-};
+(function() {
 
-drawSprite(context, imageName, x, y) {
-  if (imageName == 'pinkcar' || imageName == 'pink-car' || imageName == 'pink') {
-    context.drawImage(helpSprites, 8, 265, 30, 22, x, y, 30, 22);
-  } else if (imageName = 'bulldozer') {
-    context.drawImage(helpSprites, 9, 300, 24, 21, x, y, 24, 21);
+  alert('try loading sprites');
+  helpSprites = new Image();
+  helpSprites.src = 'https://raw.githubusercontent.com/natetarrh/frogger/master/assets/frogger_sprites.png';
+  helpSprites.onload = function() {
+    alert('sprites loaded');
+  };
+
+  frogger_drawSprite(context, imageName, x, y) {
+    if (imageName == 'pinkcar' || imageName == 'pink-car' || imageName == 'pink') {
+      context.drawImage(helpSprites, 8, 265, 30, 22, x, y, 30, 22);
+    } else if (imageName = 'bulldozer') {
+      context.drawImage(helpSprites, 9, 300, 24, 21, x, y, 24, 21);
+    }
   }
-}
+
+  window['frogger_drawSprite'] = drawSprite;
+
+})();
