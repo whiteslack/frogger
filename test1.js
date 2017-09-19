8ba655c66535627f0572d5a91c26b8a5702d2ce1
Andy Diamondstein <adiamondstein@google.com>
2017-09-19 15:46:47 -0400

    Add other sprite locations, change name to stamp

--- test1.js
+++ test1.js
@@ -4,9 +4,12 @@ helpSprites.onload = function() {
   alert('sprites loaded');
 };
 
-function draw_sprite(imageName, x, y) {
-    document.getElementById('game');
-    context = board.getContext('2d');
+function stamp(imageName, x, y) {
+    x = if (x) ? x : 0;
+    y = if (y) ? y: 0;
+    var board = document.getElementById('game');
+    var context = board.getContext('2d');
+    // Cars
     if (imageName == 'pinkcar' || imageName == 'pink-car' || imageName == 'pink') {
       context.drawImage(helpSprites, 8, 265, 30, 22, x, y, 30, 22);
     } else if (imageName == 'whitecar' || imageName == 'white-car' || imageName == 'white') {
@@ -17,5 +20,24 @@ function draw_sprite(imageName, x, y) {
       context.drawImage(helpSprites, 9, 300, 24, 21, x, y, 24, 21);
     } else if (imageName = 'truck') {
       context.drawImage(helpSprites, 105, 301, 46, 19, x, y, 46, 19);
+    // Logs
+    } else if (imageName == 'big-log' || imageName == 'biglog') {
+      context.drawImage(helpSprites, 6, 165, 179, 21, x, y, 179, 21);
+    } else if (imageName == 'medium-log' || imageName == 'mediumlog' || imageName == 'log') {
+      context.drawImage(helpSprites, 5, 197, 118, 21, x, y, 118, 21);
+    } else if (imageName == 'small-log' || imageName == 'smalllog') {
+      context.drawImage(helpSprites, 6, 229, 85, 22, x, y, 85, 22);
+    // Background
+    } else if (imageName == 'frogger-banner' || imageName == 'banner') {
+      // frogger banner goes at 0, 0
+      context.drawImage(helpSprites, 0, 0, 399, 113, x, y, 399, 113);
+    } else if (imageName == 'purple-sidewalk' || imageName == 'sidewalk') {
+      // purple sidewalk goes at 0,283 and 0,495
+      context.drawImage(helpSprites, 0, 119, 399, 34, x, y, 399, 34);
+    // frogs
+    } else if (imageName == 'safe-frog' || imageName == 'safe') {
+      context.drawImage(helpSprites, 80, 369, 23, 17, x, y, 23, 17);
+    } else if (imageName == 'frog') {
+      context.drawImage(helpSprites, 12, 369, 23, 17, x, y, 23, 17);
     }
 }
